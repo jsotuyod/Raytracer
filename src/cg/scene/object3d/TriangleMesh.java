67c293344c7b40b41e90c8cf6ea729853db2c1b7
Juan M. Sotuyo Dodero <jmsotuyo@monits.com>
2011-07-30 18:44:16 -0300

    Get rid of unused variable

--- src/cg/scene/object3d/TriangleMesh.java
+++ src/cg/scene/object3d/TriangleMesh.java
@@ -24,54 +24,50 @@ public class TriangleMesh implements Object3D {
 	public enum UVType {
 		NONE, VERTEX, FACEVARYING
 	}
-	
+
 	public enum NormalType {
 		NONE, VERTEX, FACEVARYING
 	}
-	
+
 	protected SceneTree triangles;
 	protected Shader shader;
 	protected BoundingVolume bb;
-	
-	protected Collision collidedTriangle;
-	
+
 	protected HashMap<Long, Collision> collisionMap;
-	
+
 	protected Point3D pos;
-	
+
 	public TriangleMesh(Point3D[] points, int[] vertices, Shader shader,
 			UVType uvType, float[] uvs, NormalType normalType, float[] normals, Matrix4 transform) {
 		super();
-		
+
 		if ( shader == null ) {
 			throw new NoShaderException();
 		}
-		
+
 		this.shader = shader;
-		
-		this.collidedTriangle = null;
-		
+
 		float minX, minY, minZ;
 		float maxX, maxY, maxZ;
-		
+
 		minX = minY = minZ = Float.POSITIVE_INFINITY;
 		maxX = maxY = maxZ = Float.NEGATIVE_INFINITY;
-		
+
 		Vector3D n1 = null, n2 = null, n3 = null;
 		Vector3D reference = new Vector3D(0,0,0);
-		
+
 		if ( transform != null ) {
 			reference = transform.transform(reference);
 		}
-		
+
 		List<Object3D> triangles = new LinkedList<Object3D>();
 		for (int i = 0; i < vertices.length; i+=3) {
-			
+
 			if ( normalType == NormalType.VERTEX ) {
 				n1 = new Vector3D( normals[vertices[i] * 3], normals[vertices[i] * 3 + 1], normals[vertices[i] * 3 + 2] ).normalize();
 				n2 = new Vector3D( normals[vertices[i+1] * 3], normals[vertices[i+1] * 3 + 1], normals[vertices[i+1] * 3 + 2] ).normalize();
 				n3 = new Vector3D( normals[vertices[i+2] * 3], normals[vertices[i+2] * 3 + 1], normals[vertices[i+2] * 3 + 2] ).normalize();
-				
+
 				// Transform normals!!
 				if ( transform != null ) {
 					n1 = transform.transform(n1).substract(reference).normalize();
@@ -79,7 +75,7 @@ public class TriangleMesh implements Object3D {
 					n3 = transform.transform(n3).substract(reference).normalize();
 				}
 			}
-			
+
 			if ( uvType != UVType.NONE ) {
 				triangles.add(new Triangle(points[vertices[i]], points[vertices[i+1]], points[vertices[i+2]], shader,
 							uvType, uvs[vertices[i] * 2], uvs[vertices[i] * 2 + 1], uvs[vertices[i+1] * 2], uvs[vertices[i+1] * 2 + 1],
@@ -87,7 +83,7 @@ public class TriangleMesh implements Object3D {
 			} else {
 				triangles.add(new Triangle(points[vertices[i]], points[vertices[i+1]], points[vertices[i+2]], shader, normalType, n1, n2, n3));
 			}
-			
+
 			// Check for maximum / minimums for AABB
 			for (int j = 0; j < 3; j++) {
 				if ( points[vertices[i+j]].x > maxX ) {
@@ -95,13 +91,13 @@ public class TriangleMesh implements Object3D {
 				} else if ( points[vertices[i+j]].x < minX ) {
 					minX = points[vertices[i+j]].x;
 				}
-				
+
 				if ( points[vertices[i+j]].y > maxY ) {
 					maxY = points[vertices[i+j]].y;
 				} else if ( points[vertices[i+j]].y < minY ) {
 					minY = points[vertices[i+j]].y;
 				}
-				
+
 				if ( points[vertices[i+j]].z > maxZ ) {
 					maxZ = points[vertices[i+j]].z;
 				} else if ( points[vertices[i+j]].z < minZ ) {
@@ -109,39 +105,39 @@ public class TriangleMesh implements Object3D {
 				}
 			}
 		}
-		
+
 		this.triangles = new SAHKDTree( triangles );
-		
-		this.collisionMap = new HashMap<Long, Collision>();
-		
-		this.bb = new AABB(new Point3D(minX, minY, minZ), new Point3D(maxX, maxY, maxZ));
-		
-		this.pos = new Point3D( ( minX + maxX ) * 0.5f,
+
+		collisionMap = new HashMap<Long, Collision>();
+
+		bb = new AABB(new Point3D(minX, minY, minZ), new Point3D(maxX, maxY, maxZ));
+
+		pos = new Point3D( ( minX + maxX ) * 0.5f,
 				( minY + maxY ) * 0.5f,
 				( minZ + maxZ ) * 0.5f );
 	}
 
 	@Override
 	public Color getColor(Collision collision) {
-		return this.shader.getPointColor(collision);
+		return shader.getPointColor(collision);
 	}
 
 	@Override
 	public Point3D getHitPoint(Ray r) {
-		
-		if ( this.bb.hitTest(r) == false ) {
+
+		if ( bb.hitTest(r) == false ) {
 			return null;
 		}
-		
-		Collision collision = this.triangles.hitTest(r);
-		
+
+		Collision collision = triangles.hitTest(r);
+
 		if ( collision == null ) {
 			return null;
 		}
-		
+
 		// Set it as the collision for the current thread
 		collisionMap.put(Thread.currentThread().getId(), collision);
-		
+
 		return collision.hitPoint;
 	}
 
@@ -152,7 +148,7 @@ public class TriangleMesh implements Object3D {
 
 	@Override
 	public Point3D getPos() {
-		return this.pos;
+		return pos;
 	}
 
 	@Override
@@ -167,31 +163,31 @@ public class TriangleMesh implements Object3D {
 
 	@Override
 	public float getMaxXCoord() {
-		return this.bb.getMaxP().x;
+		return bb.getMaxP().x;
 	}
 
 	@Override
 	public float getMaxYCoord() {
-		return this.bb.getMaxP().y;
+		return bb.getMaxP().y;
 	}
 
 	@Override
 	public float getMaxZCoord() {
-		return this.bb.getMaxP().z;
+		return bb.getMaxP().z;
 	}
 
 	@Override
 	public float getMinXCoord() {
-		return this.bb.getMinP().x;
+		return bb.getMinP().x;
 	}
 
 	@Override
 	public float getMinYCoord() {
-		return this.bb.getMinP().y;
+		return bb.getMinP().y;
 	}
 
 	@Override
 	public float getMinZCoord() {
-		return this.bb.getMinP().z;
+		return bb.getMinP().z;
 	}
 }
